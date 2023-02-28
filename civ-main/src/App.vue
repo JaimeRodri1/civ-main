a8a2796bcdfec22ca356d5581f51ee801506febb
JaimeRodri1 <99615467+JaimeRodri1@users.noreply.github.com>
2023-02-28 09:39:46 -0600

    aqui desscargo el civ main

---
+++ civ-main/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Navbar brand-name="Countries API" />
+  <router-view />
+  <Search @search-results="showResults"></Search>
+  <SearchResults v-if="searched" :countries="countries"></SearchResults>
+</template>
+
+<script>
+import Navbar from '../src/components/Navbar/index.vue';
+
+export default {
+  components: {
+    Navbar
+  },
+}
+</script>
